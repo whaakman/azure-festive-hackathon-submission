8cae28ef51f05b8fbc6dfe4c9374785090a79bb3
Wesley <whaakman@intercept.nl>
2020-12-23 17:34:40 +0100

    init

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "santawishlist-tf-state-rg"
-    storage_account_name = "<my-storage-account-name>"
+    storage_account_name = "santawishlisttstate324"
     container_name       = "tfstate"
     key                  = "santawishlist.terraform.tfstate"
   }
